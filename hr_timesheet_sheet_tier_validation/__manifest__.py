6de0a2043995dc6d8702802a38069636b62ba6d2
Holger Brunn <mail@hunki-enterprises.com>
2024-04-05 10:08:47 +0200

    [ADD] hr_timesheet_sheet_tier_validation

---
+++ hr_timesheet_sheet_tier_validation/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Tier validation for timesheets",
+    "summary": "Apply tier validation to timesheets",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "Hunki Enterprises BV, Odoo Community Association (OCA)",
+    "maintainers": ["hbrunn"],
+    "license": "AGPL-3",
+    "depends": [
+        "base_tier_validation",
+        "hr_timesheet_sheet",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+}
